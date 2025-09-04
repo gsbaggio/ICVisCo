6a8ef0d2b11ef9ea3eb2bfc3503105c951f873cb
Gabriel Souza Baggio <131700033+gsbaggio@users.noreply.github.com>
2025-09-04 19:42:48 -0300

    attt

--- JPEG_GreyCompression.py
+++ JPEG_GreyCompression.py
@@ -73,9 +73,68 @@ def DCT_2D(bloco): # outro jeito que quero testar
 
     return bloco_dct
 
+def transformada_DCT_blocos(blocos):
+    blocos_dct = []
+    for bloco in blocos:
+        bloco_dct = DCT_2D_metodo2(bloco)
+        blocos_dct.append(bloco_dct)
+    return blocos_dct
+
+def quantizacao(bloco_dct, matriz_quantizacao):
+    bloco_quantizado = np.round(bloco_dct / matriz_quantizacao)
+    return bloco_quantizado
+
+def quantizacao_blocos(blocos_dct, matriz_quantizacao):
+    blocos_quantizados = []
+    for bloco_dct in blocos_dct:
+        bloco_quantizado = quantizacao(bloco_dct, matriz_quantizacao)
+        blocos_quantizados.append(bloco_quantizado)
+    return blocos_quantizados
+
+def zig_zag_scan(blocos_quantizados):
+    sequencias = []
+    for bloco in blocos_quantizados:
+        sequencia = []
+        for s in range(15):
+            if s % 2 == 0:
+                for i in range(s + 1):
+                    j = s - i
+                    if i < 8 and j < 8:
+                        sequencia.append(bloco[i, j])
+            else:
+                for i in range(s + 1):
+                    j = s - i
+                    if j < 8 and i < 8:
+                        sequencia.append(bloco[j, i])
+        sequencias.append(sequencia)
+    return sequencias   
+
+def
 
 ALTURA_IMG = 128
 LARGURA_IMG = 128
+
+matriz = [
+    [16, 11, 10, 16, 24, 40, 51, 61],
+    [12, 12, 14, 19, 26, 58, 60, 55],
+    [14, 13, 16, 24, 40, 57, 69, 56],
+    [14, 17, 22, 29, 51, 87, 80, 62],
+    [18, 22, 37, 56, 68, 109, 103, 77],
+    [24, 35, 55, 64, 81, 104, 113, 92],
+    [49, 64, 78, 87, 103, 121, 120, 101],
+    [72, 92, 95, 98, 112, 100, 103, 99]
+]
+
+C = [
+    [0, 1, 5, 6, 14, 15, 27, 28],
+    [2, 4, 7, 13, 16, 26, 29, 42],
+    [3, 8, 12, 17, 25, 30, 41, 43],
+    [9, 11, 18, 24, 31, 40, 44, 53],
+    [10, 19, 23, 32, 39, 45, 52, 54],
+    [20, 22, 33, 38, 46, 51, 55, 60],
+    [21, 34, 37, 47, 50, 56, 59, 61],
+    [35, 36, 48, 49, 57, 58, 62, 63]
+]
 
 matriz_grad = criar_matriz_gradiente(ALTURA_IMG, LARGURA_IMG)
 
@@ -88,3 +147,11 @@ print(matriz_grad)
 blocos = criar_blocos_8x8(matriz_grad)
 
 print(blocos)
+
+blocos_dct = transformada_DCT_blocos(blocos)
+
+print(blocos_dct)
+
+blocos_quantizados = quantizacao_blocos(blocos_dct, np.array(matriz))
+
+print(blocos_quantizados)
