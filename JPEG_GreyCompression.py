e608adf52a99aad29b633481ac1cb7592dc75f90
Gabriel Souza Baggio <131700033+gsbaggio@users.noreply.github.com>
2025-09-10 23:30:15 -0300

    rd curve

--- JPEG_GreyCompression.py
+++ JPEG_GreyCompression.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.metrics import peak_signal_noise_ratio
 
 def imprime_matriz(matriz):
     for i in range(len(matriz)):
@@ -45,6 +46,10 @@ def obter_matriz_exemplo():
 
     return matriz_a8
 
+def gerar_matriz_aleatoria(altura, largura):
+    matriz_aleatoria = np.random.randint(0, 256, size=(altura, largura), dtype=np.uint8)
+    return matriz_aleatoria
+
 def criar_blocos_8x8(matriz):
     altura, largura = matriz.shape
     blocos = []
@@ -78,8 +83,7 @@ def DCT_matricial(blocos):
         blocos_dct.append(bloco_dct)
     return blocos_dct
 
-def cria_matriz_quantizacao():
-    R = 10
+def cria_matriz_quantizacao(R):
     matriz = np.zeros((8, 8))
     for i in range(8):
         for j in range(8):
@@ -90,8 +94,8 @@ def quantizacao(bloco_dct, matriz_quantizacao):
     bloco_quantizado = matriz_quantizacao * np.round(bloco_dct / matriz_quantizacao)
     return bloco_quantizado
 
-def quantizacao_blocos(blocos_dct):
-    matriz_quantizacao = cria_matriz_quantizacao()
+def quantizacao_blocos(blocos_dct, R):
+    matriz_quantizacao = cria_matriz_quantizacao(R)
     blocos_quantizados = []
     for bloco_dct in blocos_dct:
         bloco_quantizado = quantizacao(bloco_dct, matriz_quantizacao)
@@ -107,33 +111,65 @@ def DCT_inversa_matricial(blocos_dct):
         blocos_reconstruidos.append(bloco_reconstruido)
     return blocos_reconstruidos
 
-ALTURA_IMG = 8  
+def calcular_PSNR(matriz_original, matriz_reconstruida):
+    psnr_value = peak_signal_noise_ratio(matriz_original, matriz_reconstruida, data_range=255)
+    return psnr_value
+
+def porcentagem_zeros(blocos_quantizados):
+    total_elementos = 0
+    zeros = 0
+    for bloco in blocos_quantizados:
+        total_elementos += bloco.size
+        zeros += np.count_nonzero(bloco == 0)
+    return (zeros / total_elementos) * 100
+
+def plot_rd_curve(matriz_original, valores_R):
+    porcentagens = []
+    psnrs = []
+    for R in valores_R:
+        blocos = criar_blocos_8x8(matriz_original)
+        blocos_dct = DCT_matricial(blocos)
+        blocos_quant = quantizacao_blocos(blocos_dct, R)
+        blocos_rec = DCT_inversa_matricial(blocos_quant)
+        matriz_rec = reconstruir_matriz_de_blocos(blocos_rec, matriz_original.shape[0], matriz_original.shape[1])
+        porcentagens.append(porcentagem_zeros(blocos_quant))
+        psnrs.append(calcular_PSNR(matriz_original, matriz_rec))
+    plt.figure(figsize=(10, 6))
+    plt.plot(psnrs, porcentagens, 'o-')
+    plt.xlabel('PSNR (dB)')
+    plt.ylabel('Porcentagem de Zeros (%)')
+    plt.title('RD-Curve')
+    plt.grid(True)
+    plt.show()
+
+ALTURA_IMG = 8
 LARGURA_IMG = 8
+valores_R = [2, 5, 7, 10]
 
-matriz_grad = obter_matriz_exemplo()
+plot_rd_curve(obter_matriz_exemplo(), valores_R)
 
-print(matriz_grad)
+# matriz_grad = obter_matriz_exemplo()
 
-visualizar_matriz(matriz_grad)
+# matriz_grad = gerar_matriz_aleatoria(ALTURA_IMG, LARGURA_IMG)
 
-blocos = criar_blocos_8x8(matriz_grad)
+# print(matriz_grad)
 
-print(blocos)
+# visualizar_matriz(matriz_grad)
 
-blocos_dct_matricial = DCT_matricial(blocos)
+# blocos = criar_blocos_8x8(matriz_grad)
 
-imprime_matriz(blocos_dct_matricial)
+# blocos_dct_matricial = DCT_matricial(blocos)
 
-blocos_quantizados = quantizacao_blocos(blocos_dct_matricial)
+# blocos_quantizados = quantizacao_blocos(blocos_dct_matricial, R)
 
-imprime_matriz(blocos_quantizados)
+# print(blocos_quantizados[0])
 
-blocos_reconstruidos = DCT_inversa_matricial(blocos_quantizados)
+# blocos_reconstruidos = DCT_inversa_matricial(blocos_quantizados)
 
-imprime_matriz(blocos_reconstruidos)
+# matriz_final = reconstruir_matriz_de_blocos(blocos_reconstruidos, ALTURA_IMG, LARGURA_IMG)
 
-matriz_final = reconstruir_matriz_de_blocos(blocos_reconstruidos, ALTURA_IMG, LARGURA_IMG)
+# print(matriz_final)
 
-print(matriz_final)
+# visualizar_matriz(matriz_final)
 
-visualizar_matriz(matriz_final)
+# print("PSNR:", calcular_PSNR(matriz_grad, matriz_final))
