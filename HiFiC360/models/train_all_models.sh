a1d2a8031ca6cf6c2d32835f3e9f0d5f92bd8d0b
Gabriel Baggio <gsbaggio@inf.ufsm.br>
2025-12-17 16:46:43 -0300

    sai da reta

--- HiFiC360/models/train_all_models.sh
+++ HiFiC360/models/train_all_models.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 # filepath: /home/gabrielbaggio/Documentos/Trabalhos/ICVisCo/train_all_models.sh
 
-# # # MSE+LPIPS Low
+# # # # MSE+LPIPS Low
 # echo "Training mselpips_lo..."
-# python -m hific.train --config mselpips_lo --ckpt_dir ckpts/mse_ssim_lo_200k --num_steps 200000 --local_image_dir ../SUN360/train
+# python -m hific.train --config mselpips_lo --ckpt_dir ckpts/WSmse_WSssim_lo_200k --num_steps 200000 --local_image_dir ../SUN360/train
 
-# # # HiFiC Low
+# # # # HiFiC Low
 # echo "Training hific_lo..."
-# python -m hific.train --config hific_lo --ckpt_dir ckpts/hific_mse_ssim_lo_200k --init_autoencoder_from_ckpt_dir ckpts/mse_ssim_lo_200k --num_steps 200000 --local_image_dir ../SUN360/train
+# python -m hific.train --config hific_lo --ckpt_dir ckpts/hific_WSmse_WSssim_lo_200k --init_autoencoder_from_ckpt_dir ckpts/WSmse_WSssim_lo_200k --num_steps 200000 --local_image_dir ../SUN360/train
 
-# # MSE+LPIPS Mid
+# # # MSE+LPIPS Mid
 # echo "Training mselpips_mi..."
-# python -m hific.train --config mselpips_mi --ckpt_dir ckpts/mse_ssim_mi_200k --num_steps 200000 --local_image_dir ../SUN360/train
+# python -m hific.train --config mselpips_mi --ckpt_dir ckpts/WSmse_WSssim_mi_200k --num_steps 200000 --local_image_dir ../SUN360/train
 
 # # HiFiC Mid
-echo "Training hific_mi..."
-python -m hific.train --config hific_mi --ckpt_dir ckpts/hific_mse_ssim_mi_200k --init_autoencoder_from_ckpt_dir ckpts/mse_ssim_mi_200k --num_steps 200000 --local_image_dir ../SUN360/train
+# echo "Training hific_mi..."
+# python -m hific.train --config hific_mi --ckpt_dir ckpts/hific_WSmse_WSssim_mi_200k --init_autoencoder_from_ckpt_dir ckpts/WSmse_WSssim_mi_200k --num_steps 200000 --local_image_dir ../SUN360/train
 
 # # MSE+LPIPS High
-# echo "Training mselpips_hi..."
-# python -m hific.train --config mselpips_hi --ckpt_dir ckpts/mse_ssim_hi_200k --num_steps 200000 --local_image_dir ../SUN360/train
+echo "Training mselpips_hi..."
+python -m hific.train --config mselpips_hi --ckpt_dir ckpts/WSmse_WSssim_hi_200k --num_steps 200000 --local_image_dir ../SUN360/train
 
 # # HiFiC High
-# echo "Training hific_hi..."
-# python -m hific.train --config hific_hi --ckpt_dir ckpts/hific_mse_ssim_hi_200k --init_autoencoder_from_ckpt_dir ckpts/mse_ssim_hi_200k --num_steps 200000 --local_image_dir ../SUN360/train
+echo "Training hific_hi..."
+python -m hific.train --config hific_hi --ckpt_dir ckpts/hific_WSmse_WSssim_hi_200k --init_autoencoder_from_ckpt_dir ckpts/WSmse_WSssim_hi_200k --num_steps 200000 --local_image_dir ../SUN360/train
 
 echo "All models trained successfully!"
