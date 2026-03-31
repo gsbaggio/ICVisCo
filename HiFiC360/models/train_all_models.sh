36b61ba89009b2aeb80976d8c8517d96c317391f
Gabriel Baggio <gsbaggio@inf.ufsm.br>
2026-03-31 10:39:00 -0300

    a

--- HiFiC360/models/train_all_models.sh
+++ HiFiC360/models/train_all_models.sh
@@ -2,8 +2,8 @@
 # filepath: /home/gabrielbaggio/Documentos/Trabalhos/ICVisCo/train_all_models.sh
 
 # # # # # MSE+LPIPS Low
-echo "Training mselpips_lo..."
-python -m hific.train --config mselpips_lo --ckpt_dir ckpts/SWHDC_WSmse_WSssim_256x512_lo_1M --num_steps 1000000 --crop_height 512 --crop_width 256 --local_image_dir ../SUN360/train
+# echo "Training mselpips_lo..."
+# python -m hific.train --config mselpips_lo --ckpt_dir ckpts/SWHDC_WSmse_WSssim_256x512_lo_1M --num_steps 1000000 --crop_height 512 --crop_width 256 --local_image_dir ../SUN360/train
 
 # # # # # HiFiC Low
 # echo "Training hific_lo..."
@@ -18,8 +18,8 @@ python -m hific.train --config mselpips_mi --ckpt_dir ckpts/SWHDC_WSmse_WSssim_2
 # python -m hific.train --config hific_mi --ckpt_dir ckpts/gauss_hific_WSmse_WSssim_mi_200k --init_autoencoder_from_ckpt_dir ckpts/gauss_WSmse_WSssim_mi_200k --num_steps 200000 --local_image_dir ../SUN360/train
 
 # # MSE+LPIPS High
-echo "Training mselpips_hi..."
-python -m hific.train --config mselpips_hi --ckpt_dir ckpts/SWHDC_WSmse_WSssim_256x512_hi_1M --num_steps 1000000 --crop_height 512 --crop_width 256 --local_image_dir ../SUN360/train
+# echo "Training mselpips_hi..."
+# python -m hific.train --config mselpips_hi --ckpt_dir ckpts/SWHDC_WSmse_WSssim_256x512_hi_1M --num_steps 1000000 --crop_height 512 --crop_width 256 --local_image_dir ../SUN360/train
 
 # # HiFiC High
 # echo "Training hific_hi..."
